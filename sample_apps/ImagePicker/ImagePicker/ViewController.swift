9d1c7621fe16ca029e492fb7af60924c5d64c560
ydavidchen <youdinghuan.chen.gr@dartmouth.edu>
2020-03-28 13:14:15 -0400

    ImagePicker sample app: alert appear & dismiss fxns added

--- sample_apps/ImagePicker/ImagePicker/ViewController.swift
+++ sample_apps/ImagePicker/ImagePicker/ViewController.swift
@@ -10,17 +10,36 @@ import UIKit;
 
 class ViewController: UIViewController {
     //MARK: - Properties
-    @IBOutlet weak var showGalleryButton: UIButton!
+    @IBOutlet weak var showGalleryButton: UIButton!;
+    @IBOutlet weak var showAlertButton: UIButton!;
+    
     
     //MARK: - Built-in Methods
     override func viewDidLoad() {
         super.viewDidLoad();
     }
     
-    //MARK: - Custom action/methods
-    @IBAction func experiment() {
+    
+    //MARK: - Custom actions/methods
+    @IBAction func showGallery() {
         let nextController = UIImagePickerController();
         present(nextController, animated: true, completion: nil);
+    }
+    
+    @IBAction func showAlert() {
+        let alertControler = UIAlertController();
+        
+        // Create alert:
+        alertControler.title = "TEST ALERT";
+        alertControler.message = "This is a TEST";
+        
+        // Give user an option to close alert via a callback:
+        let okAction = UIAlertAction(title:"OK", style: UIAlertAction.Style.default){
+            action in self.dismiss(animated:true, completion: nil)
+        };
+        alertControler.addAction(okAction);
+        
+        present(alertControler, animated: true, completion: nil);
     }
 }
 
