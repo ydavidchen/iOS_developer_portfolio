e09283f5f4f60b095166f4e02d1a38f58570202e
ydavidchen <youdinghuan.chen.gr@dartmouth.edu>
2020-03-28 11:10:54 -0400

    ColorMaker sample app: Update from binary Switches to continuous Sliders

--- sample_apps/ColorMaker/ColorMaker/ViewController.swift
+++ sample_apps/ColorMaker/ColorMaker/ViewController.swift
@@ -14,9 +14,9 @@ class ViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var redControl: UISwitch!
-    @IBOutlet weak var greenControl: UISwitch!
-    @IBOutlet weak var blueControl: UISwitch!
+    @IBOutlet weak var redControl: UISlider! //UISwitch if binary
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
 
     // MARK: - Activity lifecycle
     override func viewDidLoad() {
@@ -27,10 +27,17 @@ class ViewController: UIViewController {
     // MARK: - Actions as methods
     @IBAction func changeColorComponent() {        
         if self.redControl == nil {return}
-        let r: CGFloat = self.redControl.isOn ? 1 : 0;
-        let g: CGFloat = self.greenControl.isOn ? 1 : 0;
-        let b: CGFloat = self.blueControl.isOn ? 1 : 0;
+        
+        // Binary RGB switches: DO NOT DELETE - KEEP AS REFERENCE
+        // let r: CGFloat = self.redControl.isOn ? 1 : 0;
+        // let g: CGFloat = self.greenControl.isOn ? 1 : 0;
+        // let b: CGFloat = self.blueControl.isOn ? 1 : 0;
                 
+        // Continuous RGB slider: NAMED SAME AS BINARY SWITCHES
+        let r: CGFloat = CGFloat(self.redControl.value);
+        let g: CGFloat = CGFloat(self.greenControl.value);
+        let b: CGFloat = CGFloat(self.blueControl.value);
+        
         colorView.backgroundColor = UIColor(red:r, green:g, blue:b, alpha:1);
     }
 }
