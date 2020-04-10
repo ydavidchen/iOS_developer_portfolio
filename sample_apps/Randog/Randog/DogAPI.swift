48ea35b0319a4cd24794838dd0fa43e1fbaa50c9
ydavidchen <youdinghuan.chen.gr@dartmouth.edu>
2020-04-10 18:10:49 -0400

    Randog sample app: important code refactoring w.r.t. completion handlers

--- sample_apps/Randog/Randog/DogAPI.swift
+++ sample_apps/Randog/Randog/DogAPI.swift
@@ -4,6 +4,7 @@
 //  Copyright © 2020 DavidKevinChen. All rights reserved.
 
 import Foundation;
+import UIKit;
 
 class DogAPI {
     //MARK: - Each case is an endpoint
@@ -15,4 +16,46 @@ class DogAPI {
             return URL(string:self.rawValue)!; //unwrapping required
         }
     }
+    
+    //MARK: - Request random image
+    class func requestRandomImage(completionHandler: @escaping (DogImage?, Error?) -> Void) {
+
+        let randomImageEndpoint = DogAPI.Endpoint.randomImageFromAllDogsCollection.url;
+        print(randomImageEndpoint);
+        
+        let task = URLSession.shared.dataTask(with:randomImageEndpoint){(data, response, error) in
+            guard let data = data else {
+                completionHandler(nil, error);
+                return;
+            }
+            print(data);
+
+            let decoder = JSONDecoder();
+            let imageData = try! decoder.decode(DogImage.self, from:data);
+                   print(imageData);
+            completionHandler(imageData, nil);
+        }
+        task.resume();
+        
+    }
+    
+    //MARK: - API request procedures
+    class func requestImageFile(imageUrl:URL, completionHandler: @escaping (UIImage?,Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with:imageUrl, completionHandler: {(data, response, error) in
+            
+            // Scenario 1: Error!=nil && Image=nil
+            guard let data = data else {
+                print("Failed to fetch data from API");
+                completionHandler(nil, error);
+                return;
+            }
+            
+            // Scenario 2: Image!=nil WHEN Error=nil
+            let downloadedImg = UIImage(data:data); //overloaded constructor
+            completionHandler(downloadedImg, nil);
+        });
+        
+        task.resume();
+    }
+    
 }
--- sample_apps/Randog/Randog/ViewController.swift
+++ sample_apps/Randog/Randog/ViewController.swift
@@ -1,11 +1,10 @@
 //  ViewController.swift
 //  Randog
-//
 //  Created by DavidKevinChen on 4/10/20.
 //  Copyright © 2020 DavidKevinChen. All rights reserved.
-//
 
-import UIKit
+import UIKit;
+import Foundation;
 
 class ViewController: UIViewController {
 
@@ -13,54 +12,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        
-        let randomImageEndpoint = DogAPI.Endpoint.randomImageFromAllDogsCollection.url;
-        print(randomImageEndpoint);
-        
-        let task = URLSession.shared.dataTask(with: randomImageEndpoint) {(data, response, error) in
-            
-            // Closure -- code/fxn to run when complete
-            guard let data = data else  {
-                return;
-            }
-            print(data);
-            
-            //SERIALISATION approach: Parse retrieved JSON data - OLD school method
-//            do {
-//                let json = try JSONSerialization.jsonObject(with:data, options:[]) as! [String: Any];
-//                let url = json["message"] as! String;
-//                print(url);
-//            } catch {
-//                print(error);
-//            }
-            
-            //CODABLE approach: Decode into custom type
-            let decoder = JSONDecoder();
-            let imageData = try! decoder.decode(DogImage.self, from:data);
-            print(imageData);
-            
-            //Download image & set to imageview via Main thread in a NESTED TASK
-            guard let imageUrl = URL(string: imageData.message) else {return;};
-            
-            let innerTask = URLSession.shared.dataTask(with:imageUrl, completionHandler: {(data, response, error) in
-                
-                guard let data = data else {
-                    print("Failed to fetch data");
-                    return;
-                }
-                let downloadedImg = UIImage(data:data); //overloaded constructor
-                DispatchQueue.main.async {
-                    self.imageView.image = downloadedImg;
-                }
-            });
-            innerTask.resume();
-            
-        }
-        
-        
-        task.resume();
+        DogAPI.requestRandomImage(completionHandler:handleRandomImgResponse(imageData:error:));
     }
-
+    
+    //MARK: - Abstracted method for accessing API (outer async task)
+    func handleRandomImgResponse(imageData:DogImage?, error:Error?) {
+        guard let imageUrl = URL(string:imageData?.message ?? "") else {return;};
+        DogAPI.requestImageFile(imageUrl:imageUrl, completionHandler:self.handleImgFileResponse(image:error:));
+    }
+    
+    func handleImgFileResponse(image:UIImage?, error:Error?) {
+        DispatchQueue.main.async {
+            self.imageView.image = image;
+        }
+    }
 
 }
 
